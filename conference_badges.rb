073a9fa20764ef14ac5f8d04c1c1b81c4fa69030
Matt Pitner <mattpitner09@gmail.com>
2019-08-15 18:00:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  arr = []
+  attendees.each do |attendee|
+    arr.push("Hello, my name is #{attendee}.")
+  end 
+  return arr
+end 
+
+def assign_rooms(attendees)
+
+
+end
