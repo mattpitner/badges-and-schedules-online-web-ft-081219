04585ac9a708d7a09b7832e8213cf573319bdde7
Matt Pitner <mattpitner09@gmail.com>
2019-08-15 18:35:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(name)
       rooms_assigments.push( "Hello, #{list}! You'll be assigned to room #{index + 1 }!")
       end
        return rooms_assigments
-     end
+     
  
     
 def printer(attendees)
